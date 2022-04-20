47ebb9aea7ee5ced1490d9f2ff6395cb285b57a5
ericorizky <ericorizky@students.amikom.ac.id>
2022-04-20 10:52:47 +0700

    ResponsiPemrograman

---
+++ Karyawan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4452
+{
+    internal class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public float Gajibulanan { get; set; }
+        public Karyawan(string Nik, string nama, float gaji)
+        {
+            this.Nik = Nik;
+            this.Nama = nama;
+            if (gaji < 0)
+            {
+                gaji = 0;
+            }
+            this.Gajibulanan = gaji;
+        }
+
+        public void NaikkanGaji(float persentase)
+        {
+            this.Gajibulanan = persentase / 100 * this.Gajibulanan + this.Gajibulanan;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4452
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("12345", "Erico", 2000000);
+            Karyawan karyawan2 = new Karyawan("67890", "Rizky", 3000000);
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", karyawan1.Nik, karyawan1.Nama, karyawan1.Gajibulanan);
+            Console.WriteLine("2    {0}/{1}         {2}", karyawan2.Nik, karyawan2.Nama, karyawan2.Gajibulanan);
+
+            Console.WriteLine();
+            Console.WriteLine("Asyiiik kenaikan gaji sebesar 10% !!");
+            Console.WriteLine();
+
+            karyawan1.NaikkanGaji(10);
+            karyawan2.NaikkanGaji(10);
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", karyawan1.Nik, karyawan1.Nama, karyawan1.Gajibulanan);
+            Console.WriteLine("2    {0}/{1}         {2}", karyawan2.Nik, karyawan2.Nama, karyawan2.Gajibulanan);
+
+            Console.ReadKey();
+        }
+    }
+}
